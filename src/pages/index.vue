a5b1506acabba258f3f8166a5e0057d6507cde0e
sandnima <sandnima33@gmail.com>
2021-12-19 16:35:24 +0330

    - default layout changed - index page changed - font name set

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,38 +7,38 @@
       </v-card>
       <v-card>
         <v-card-title class="headline">
-          خوش آمدید
+          {{ 'خوش آمدید' }}
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>
             For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
+            href="https://vuetifyjs.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            documentation
+          </a>.
           </p>
           <p>
             If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
+            href="https://chat.vuetifyjs.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="chat"
+          >
+            discord
+          </a>.
           </p>
           <p>
             Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
+            href="https://github.com/vuetifyjs/vuetify/issues"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="contribute"
+          >
+            issue board
+          </a>.
           </p>
           <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
           <div class="text-xs-right">
@@ -75,3 +75,6 @@
     </v-col>
   </v-row>
 </template>
+
+<script>
+</script>
